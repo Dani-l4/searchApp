0558992406de5355155f83d25f1b0ddc40624a90
Mirash <mirash.ru137@mail.ru>
2024-02-29 13:41:20 +0300

    move modal js to main script

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', async function() {
       </li>`;
       $posts.insertAdjacentHTML('beforeend', post_card);
     })
-    document.dispatchEvent(new Event('fetchedNbuilded'));
+    setCardsClickEvent();
   }
 
   let posts = await fetchPosts();
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', async function() {
 
     renderPosts(searchResult);
 
-    items = Array.from($posts.getElementsByTagName('li')).slice(0)
+    items = Array.from($posts.getElementsByTagName('li'))
 
     createPageButtons();
     showPage(currentPage)
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', async function() {
 
   const itemsPerPage = 8;
   let currentPage = 0;
-  let items = Array.from($posts.getElementsByTagName('li')).slice(0)
+  let items = Array.from($posts.getElementsByTagName('li'))
 
   function showPage(page) {
     const startIndex = page * itemsPerPage;
@@ -91,5 +91,58 @@ document.addEventListener('DOMContentLoaded', async function() {
   }
 
   createPageButtons();
-  showPage(currentPage)
+  showPage(currentPage);
+
+  // MODAL ==================
+
+  const $modal = document.getElementById('modal');
+  const $form = document.querySelector('.form');
+
+  function openModal() {
+    document.body.classList.add('lock');
+    $modal.classList.add('open');
+  }
+  
+  function closeModal() {
+    document.body.classList.remove('lock');
+    $modal.classList.remove('open');
+  }
+
+  $modal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('form__close_btn') || e.target.classList.contains('modal__body')) {
+      closeModal()
+    }
+  });
+  
+  document.addEventListener('keydown', function(e) { 
+    if (e.key === 'Escape') {
+      closeModal()
+    }
+  })
+
+  function setCardsClickEvent() {
+    let cards = document.querySelectorAll('.card');
+    
+    for (let card of cards){
+      card.addEventListener('click', editPost)
+    }
+  }
+
+  function editPost(e) {
+    const card = this;
+    const $postId = $form.elements['post_id'];
+    const $postTitle = $form.elements['post_title'];
+    const $postBody = $form.elements['post_body'];
+  
+    $postId.value = card.dataset.id;
+    $postTitle.value = card.dataset.title;
+    $postBody.value = card.dataset.body;
+  
+    $postBody.style.height = $postBody.scrollHeight + 'px';
+  
+    openModal()
+  
+    e.preventDefault()
+  }
+  
 })
